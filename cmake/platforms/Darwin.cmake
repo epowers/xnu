0e8e19616e1908a44333daf9769260e2a7a8b1c7
Eric Powers <eric@mrpowers.com>
2013-04-07 00:34:42 -0700

    Initial CMake installs mach.h

---
+++ cmake/platforms/Darwin.cmake
@@ -0,0 +1,13 @@
+# cmake might set this by default when this target file is used:
+#set(CMAKE_CROSSCOMPILING TRUE)
+
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_VERSION 10.8)
+set(CMAKE_SYSTEM_PROCESSOR "i686")
+set(CMAKE_FIND_ROOT_PATH /usr/i686-apple-darwin10.8.0)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# This appears to be ignored
+#set(CMAKE_INSTALL_PREFIX /usr/i686-apple-darwin10.8.0 FORCE)
